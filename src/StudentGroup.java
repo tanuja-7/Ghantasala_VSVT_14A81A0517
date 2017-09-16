86e17178a7bbf79bbb81fb4ac59f59b165ec76e2
tanuja-7 <32010956+tanuja-7@users.noreply.github.com>
2017-09-16 14:50:55 +0530

    Update StudentGroup.java

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -26,20 +26,24 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public Student[] getStudents() {
-		if(students == null)
-			throw new IllegalArgumentException(" ");
-		else
 			return students;
 	}
 
 	@Override
 	public void setStudents(Student[] students) {
 		// Add your implementation here
+		if(students==null)
+			throw new IllegalArgumentException(" ");
+		else
+			this.students=students;
 	}
 
 	@Override
 	public Student getStudent(int index) {
-		// Add your implementation here
+		if(index<0 ||(index>=students.length))	
+			throw new IllegalArgumentException(" ");
+		else
+			return students[i];
 		return null;
 	}
 
